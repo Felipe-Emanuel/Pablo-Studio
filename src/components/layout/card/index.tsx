5b65b14ebe28bb46f3e613c375a56e8d50c1e85d
Felipe <felipesullivan50@gmail.com>
2023-03-14 17:03:23 -0300

    ✨ feat: Static card
    add a static responsive and dynamic card

---
+++ src/components/layout/card/index.tsx
@@ -0,0 +1,38 @@
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+
+interface CardProps {
+  img: StaticImageData;
+  alt: string;
+}
+
+export function Card({ img, alt }: CardProps) {
+  const [isOverlay, setIsOverlay] = useState(true);
+
+  const setOverlay = () => setIsOverlay((isOverlay) => !isOverlay);
+  const checkOverlay = isOverlay ? "bg-dark/50" : "bg-transparent";
+
+  return (
+    <div
+      onMouseEnter={setOverlay}
+      onMouseLeave={setOverlay}
+      className="
+      relative rounded-3xl p-1 w-56 sm:w-80 h-72 sm:h-[468px] border
+      border-transparent hover:border-secondary transition-all"
+    >
+      <div
+        className={`transition-all absolute inset-1 rounded-3xl ${checkOverlay}`}
+      />
+      <Image
+        placeholder="blur"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOMDYutBwADuQGRo8ohBwAAAABJRU5ErkJggg=="
+        src={img}
+        alt={alt}
+        width={320}
+        height={468}
+        quality={100}
+        className={`w-full h-full rounded-3xl`}
+      />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@util/buttons/Button";
+import { Card } from "@layout/card";
+import img from '../../public/a.png'
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col gap-4 bg-dark/50">
-      <Button cart text="Adicionar ao carrinho" />
+    <div className="w-screen h-screen flex flex-col gap-4 bg-dark">
+      <Card alt="a" img={img} />
     </div>
   );
 }
