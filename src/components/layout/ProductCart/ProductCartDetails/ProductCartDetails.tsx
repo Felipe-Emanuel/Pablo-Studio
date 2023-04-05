2e25788b9db3357d2bc79f6b88ac4aa6d821e96f
Felipe <felipesullivan50@gmail.com>
2023-04-04 22:50:16 -0300

    🌈 style: CardDetails styled
    change remove button and price color styles

--- src/components/layout/ProductCart/ProductCartDetails/ProductCartDetails.tsx
+++ src/components/layout/ProductCart/ProductCartDetails/ProductCartDetails.tsx
@@ -1,3 +1,4 @@
+import { normalize } from "@functions/normalized";
 import { useCartContext } from "@hooks/useCartContext";
 import { SelectedCardProps } from "@layout/selectedCard";
 import { Price } from "@util/texts/Price";
@@ -7,10 +8,11 @@ import { useState } from "react";
 import { LeftArrowIcon, RightArrowIcon } from "src/icons";
 
 interface ProductCartDetailsProps {
-  product: SelectedCardProps
+  product: SelectedCardProps;
 }
 
-export function ProductCartDetails({product}: ProductCartDetailsProps) {
+export function ProductCartDetails({ product }: ProductCartDetailsProps) {
+  const { formatPrice } = normalize();
   const { totalWithPix, removeFromCart } = useCartContext();
   const [qtd, setQtd] = useState(0);
 
@@ -26,24 +28,33 @@ export function ProductCartDetails({product}: ProductCartDetailsProps) {
     <div className="flex flex-col justify-between w-fit h-full py-8 gap-3">
       <span className="flex flex-col items-end">
         <Text light text="Preço à vista no PIX" className="text-end" />
-        <Price price={totalWithPix} className="text-secondary" />
+        <p className="font-bold text-secondary text-end">
+          {formatPrice(totalWithPix)}
+        </p>
       </span>
       <div className="flex flex-col items-center justify-center gap-3">
         <Text text="Quantidade" light />
         <div className="relative w-full items-center justify-center">
           <LeftArrowIcon
             onClick={removeQtd}
-            className={`absolute transition-all cursor-pointer left-0 w-5 h-7 text-white hover:text-secondary ${checkQtd}`}
+            className={`
+              absolute transition-all cursor-pointer left-0
+              w-5 h-7 text-white hover:text-secondary ${checkQtd}`}
           />
           <RightArrowIcon
             onClick={addQtd}
-            className={`absolute transition-all cursor-pointer right-0 w-5 h-7 text-white hover:text-secondary`}
+            className={`
+              absolute transition-all cursor-pointer right-0
+              w-5 h-7 text-white hover:text-secondary`}
           />
           <Text text={qtd} className="text-center" />
         </div>
-        <button onClick={() => removeFromCart(product)} className="flex gap-1.5 items-center justify-center transition-all hover:opacity-75">
+        <button
+          onClick={() => removeFromCart(product)}
+          className="flex gap-1.5 items-center justify-center transition-all hover:opacity-75"
+        >
           <TrashVector />
-          <Text bold text="Remover" className="text-[#C03131]" />
+          <p className="font-bold text-danger">Remover</p>
         </button>
       </div>
     </div>
