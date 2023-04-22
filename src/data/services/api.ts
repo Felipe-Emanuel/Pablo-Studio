4572fbc9f6df57818ccbba4aa1d925b9c04503ca
Felipe <felipesullivan50@gmail.com>
2023-04-22 12:38:04 -0300

    🦄 refactor: merge conflicts
    resolve a merge conflict with baseURL

--- src/data/services/api.ts
+++ src/data/services/api.ts
@@ -1,26 +1,7 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-// const api = axios.create({
-//   baseURL: 'https://pablo-studio.vercel.app/'
-// })
-
-// export default api
-
-//DEV API
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000/'
-})
-
-export default api
-=======
 const api = axios.create({
   baseURL: 'https://api-correios-request.vercel.app'
 })
 
 export default api
-
-
-// https://api-correios-request.vercel.app
->>>>>>> 96be41c
